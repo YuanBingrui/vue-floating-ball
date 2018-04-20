445d066515708d27c52902857856f1995fb35016
袁秉瑞 <bingrui.yuan@roadshare.com>
2018-04-20 17:40:01 +0800

    add floatingBallPopover component

---
+++ src/components/FloatingBallPopover.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="floating-ball-popover"></div>
+</template>
+
+<script>
+export default {
+  name: 'FloatingBallPopover',
+  data () {
+    return {
+
+    }
+  },
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.floating-ball-popover {
+  position: absolute;
+  border-radius: 2rem;
+  // width: 0;
+  // height: 0;
+  // transition: width 2s, height 2s;
+}
+</style>
